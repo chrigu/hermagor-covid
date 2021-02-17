eceeb4831d300fb4872b2e02b3b7abaa45409e43
Christian Cueni <christian.cueni@iterativ.ch>
2021-02-17 20:42:04 +0100

    Rename chart component

--- covid-web/src/views/Home.vue
+++ covid-web/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <LineChart :chart-data="data" :options="options" />
+    <BarChart :chart-data="data" :options="options" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import LineChart from '@/components/LineChart.vue'
+import BarChart from '@/components/BarChart.vue'
 import covidData from '@/assets/data.json'
 
 export default {
   name: 'Home',
   components: {
-    LineChart
+    BarChart
   },
   data () {
     return {
