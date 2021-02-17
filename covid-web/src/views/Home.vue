d693a101269be3f60d96f38c8c44232fab918c06
Christian Cueni <christian.cueni@iterativ.ch>
2021-02-17 21:22:45 +0100

    Add sum charts

--- covid-web/src/views/Home.vue
+++ covid-web/src/views/Home.vue
@@ -6,23 +6,26 @@
       <li>{{latestDeaths}}</li>
       <li>{{latestIncidence}}</li>
     </ul>
-    <BarChart v-if="hasDatasets" :chart-data="data" :options="options" />
+    <BarChart v-if="hasDatasets(dailyData)" :chart-data="dailyData" :options="dailyOptions" />
+    <LineChart v-if="hasDatasets(sumData)" :chart-data="sumData" :options="sumOptions" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import BarChart from '@/components/BarChart.vue'
+import LineChart from '@/components/LineChart'
 import covidData from '@/assets/data.json'
 
 export default {
   name: 'Home',
   components: {
-    BarChart
+    BarChart,
+    LineChart
   },
   data () {
     return {
-      options: {
+      dailyOptions: {
         title: {
           display: true,
           text: 'Hermagor neue Fälle & Tote'
@@ -34,7 +37,23 @@ export default {
           }]
         }
       },
-      data: {
+      dailyData: {
+        labels: ['bla', 'blu'],
+        datasets: []
+      },
+      sumOptions: {
+        title: {
+          display: true,
+          text: 'Hermagor neue Fälle & Tote'
+        },
+        scales: {
+          xAxes: [{
+            type: 'time',
+            distribution: 'series'
+          }]
+        }
+      },
+      sumData: {
         labels: ['bla', 'blu'],
         datasets: []
       },
@@ -44,36 +63,36 @@ export default {
       latestIncidence: -1
     }
   },
-  computed: {
-    hasDatasets () {
-      return this.data.datasets.length > 0
-    }
-  },
   methods: {
-    createDates (series) {
-      return series.map((datapoint) => new Date(datapoint.x))
-    },
-    createDataSet (data, dates, label, color) {
+    // createDates (series) {
+    //   return series.map((datapoint) => new Date(datapoint.x))
+    // },
+    createDataSet (data, label, color) {
       return {
         label: label,
         fill: false,
-        data: data.map((datapoint, i) => ({ x: dates[i], y: datapoint.y })),
+        data: data.map((datapoint, i) => ({ x: datapoint.x, y: datapoint.y })),
         backgroundColor: color
       }
     },
     getLatestDatapoint (data) {
       return data.slice(-1)[0]
+    },
+    hasDatasets (data) {
+      return data.datasets.length > 0
     }
   },
   mounted () {
-    const dates = this.createDates(covidData.cases)
-    const casesSet = this.createDataSet(covidData.cases, dates, 'Fälle', '#FFFF00')
-    const deathsSet = this.createDataSet(covidData.deaths, dates, 'Tote', '#FF0000')
-    console.log(deathsSet)
-    this.data.datasets = [casesSet, deathsSet]
-    console.log(this.data)
+    // const dates = this.createDates(covidData.cases)
+    const dailyCasesSet = this.createDataSet(covidData.cases, 'Fälle', '#FFFF00')
+    const dailyDeathsSet = this.createDataSet(covidData.deaths, 'Tote', '#FF0000')
+    this.dailyData.datasets = [dailyCasesSet, dailyDeathsSet]
 
-    this.latestDate = this.getLatestDatapoint(dates)
+    const sumCasesSet = this.createDataSet(covidData.cases_sum, 'Fälle', '#FFFF00')
+    const sumDeathsSet = this.createDataSet(covidData.deaths_sum, 'Tote', '#FF0000')
+    this.sumData.datasets = [sumCasesSet, sumDeathsSet]
+
+    this.latestDate = this.getLatestDatapoint(covidData.cases).x
     this.latestCases = this.getLatestDatapoint(covidData.cases).y
     this.latestDeaths = this.getLatestDatapoint(covidData.deaths).y
     this.latestIncidence = this.getLatestDatapoint(covidData.incidence).y
